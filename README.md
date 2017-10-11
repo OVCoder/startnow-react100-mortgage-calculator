01b4de6e34ca79a32f99bdfe358ebb098d9906c8
OVCoder <citryn85@gmail.com>
2017-10-11 16:14:09 -0700

    adding original files for the project

---
+++ README.md
@@ -0,0 +1,9 @@
+Project Description:
+
+This is a mortgage calculator used with React.
+
+Setup steps:
+
+1) npm install
+2) npm run build
+3) npm start
---
+++ test/calculator.spec.js
@@ -0,0 +1,11 @@
+/* global define, it, describe */
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import App from './../src/js/app';
+
+describe('App Component', function () {
+  this.timeout(10000);
+  this.slow(3000);
+
+});
